02f416830e8c45bfda74dbdd31040165c06109fb
Alison Lubar <aslubar@gmail.com>
2021-01-19 20:41:09 -0500

    API call is successful when using the search box

--- src/components/EmployeeContainer.js
+++ src/components/EmployeeContainer.js
@@ -8,21 +8,36 @@ import EmployeeDetail from "./EmployeeDetail";
 import API from "../utils/API";
 
 class EmployeeContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.searchEmployees = this.searchEmployees.bind(this)
+  }
   state = {
     result: {},
     search: ""
   };
 
-  // When this component mounts, search for the movie "The Matrix"
+  // When this component mounts, search for an employee
   componentDidMount() {
-    this.searchEmployees("Name Name");
+    this.getEmployees();
   }
 
-  searchEmployees = query => {
-    API.search(query)
-      .then(res => this.setState({ result: res.data }))
+  //this is where we need API.EmployeeDetail().then...the rest of line 24 and onward?
+  getEmployees = () => {
+    API.search()
+      .then(res => { console.log(res); return this.setState({ result: res.data.results }) })
       .catch(err => console.log(err));
   };
+
+  //add the function to search employees-- will probably be a filter, to filter through the state
+  searchEmployees = (searchTerm) => {
+    this.state.result.filter((item) => {
+      console.log(item)
+      //this is where you'll have the logic for comparing, JS comparison function
+    })
+    //this returns an array of the items that match the query
+
+  }
 
   handleInputChange = event => {
     const value = event.target.value;
